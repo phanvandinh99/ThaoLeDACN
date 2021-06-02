d3518fbabc888224963a29035a4decf45e0531f9
DinhPV <phanvandinh999@gmail.com>
2021-06-02 13:53:57 +0700

    Add new KhachHang>>NhanVien CRUD

---
+++ BookStore/Areas/Admin/Controllers/NhanVienController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        private QuanLySachEntities db = new QuanLySachEntities();
+
+        // GET: Admin/NhanVien
+        public ActionResult Index()
+        {
+            return View(db.NhanViens.ToList());
+        }
+
+        // GET: Admin/NhanVien/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NhanVien nhanVien = db.NhanViens.Find(id);
+            if (nhanVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhanVien);
+        }
+
+        // GET: Admin/NhanVien/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/NhanVien/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TaiKhoanNV,MatKhauNV,TenNhanVien,NgaySinh,GioiTinh,Email,SoDienThoai,QueQuan,Quyen")] NhanVien nhanVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NhanViens.Add(nhanVien);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(nhanVien);
+        }
+
+        // GET: Admin/NhanVien/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NhanVien nhanVien = db.NhanViens.Find(id);
+            if (nhanVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhanVien);
+        }
+
+        // POST: Admin/NhanVien/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TaiKhoanNV,MatKhauNV,TenNhanVien,NgaySinh,GioiTinh,Email,SoDienThoai,QueQuan,Quyen")] NhanVien nhanVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nhanVien).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nhanVien);
+        }
+
+        // GET: Admin/NhanVien/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NhanVien nhanVien = db.NhanViens.Find(id);
+            if (nhanVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhanVien);
+        }
+
+        // POST: Admin/NhanVien/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            NhanVien nhanVien = db.NhanViens.Find(id);
+            db.NhanViens.Remove(nhanVien);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
